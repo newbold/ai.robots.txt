9a8d71afe1075ace18c146816f33bddb8380ff41
Adam Newbold <adam@neatnik.net>
2024-08-01 17:32:57 -0400

    does this show up

--- code/action.php
+++ code/action.php
@@ -4,7 +4,17 @@
 
 $robots = json_decode(file_get_contents('robots.json'), 1);
 
-file_put_contents(time(), print_r($robots, 1));
+$robots_txt = null;
+
+foreach($robots as $robot => $details) {
+  
+  $robots_txt .= 'User-agent: '.$robot."\n";
+  
+}
+
+$robots_txt .= 'Disallow: /';
+
+file_put_contents('robots.txt', $robots_txt);
 
 
 
