75c7e18a98fe027acbd3eda23879986586286c68
Adam Newbold <adam@neatnik.net>
2024-08-01 17:36:30 -0400

    blargh

--- code/action.php
+++ code/action.php
@@ -14,8 +14,11 @@ foreach($robots as $robot => $details) {
 
 $robots_txt .= 'Disallow: /';
 
+unlink('robots.txt');
+
 file_put_contents('robots.txt', $robots_txt);
-file_put_contents('robots2.txt', $robots_txt);
+
+//file_put_contents('robots2.txt', $robots_txt);
 
 
 
